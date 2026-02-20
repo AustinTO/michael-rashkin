7e9bc366a8f4a82c271f131163b080feb38afe23
AustinTO <AustinTaylorODell@gmail.com>
2026-02-20 12:27:11 -0500

    Enable React renderer for embedded Sanity Studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
 import dotenv from 'dotenv';
 import sanity from '@sanity/astro';
 
@@ -17,6 +18,7 @@ export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
   integrations: [
+    react(),
     sanity({
       projectId,
       dataset,
